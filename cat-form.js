c5142e77ef6b31312f2994fd9b648c1e0c16b8b5
“BethFieldgit config --global user.email “bf123@hotmail.co.uk <bf123@hotmail.co.uk>
2022-10-06 14:28:11 +0100

    changes to cat form html and styling

---
+++ cat-form.js
@@ -0,0 +1,60 @@
+let breedCount = { value: 0 };
+let tempCount = { value: 0 };
+
+// extract data from form and send to server
+async function sendUserData(e) {
+  e.preventDefault();
+  await validation();
+  //   console.log(validated);
+  const data = {
+    birthDate: e.target.birthDate.value,
+    location: e.target.location.value,
+    accommodation: e.target.accommodation.value,
+    housemates: e.target.housemates.value,
+    children: e.target.children.value,
+    travel: e.target.travel.value,
+    exercise: e.target.exercise.value,
+    walks: e.target.walks.value,
+    workHome: e.target.workHome.value,
+    dogExperience: e.target.dogExperience.value,
+    numberOfPets: e.target.numberOfPets.value,
+    dogAllergy: e.target.dogAllergy.value,
+    dogMeds: e.target.dogMeds.value,
+    breed: filterCheckboxes(e.target.breed),
+    houseCat: e.target.houseCat.value,
+    temperament: filterCheckboxes(e.target.temperament),
+  };
+
+  if (
+    breedCount.value > 0 &&
+    breedCount.value < 3 &&
+    tempCount.value > 0 &&
+    tempCount.value < 3
+  ) {
+    sessionStorage.setItem("formData", JSON.stringify(data));
+    window.location.href = `./response.html`;
+  } else {
+    alert("Please");
+  }
+}
+
+function validation() {
+  let breedBoxes = document.querySelectorAll(".breedCheck");
+  let tempBoxes = document.querySelectorAll(".tempCheck");
+  breedCount.value = 0;
+  tempCount.value = 0;
+
+  countCheckbox(breedBoxes, breedCount);
+  countCheckbox(tempBoxes, tempCount);
+}
+
+function countCheckbox(checkboxes, count) {
+  checkboxes.forEach((box) => {
+    if (box.checked) {
+      count.value += 1;
+    }
+  });
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", sendUserData);
